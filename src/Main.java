f48372d15d384e0c4a421f82a9f2f5652225f216
Alan-Ren1 <alanr94@nycstudents.net>
2025-11-24 15:13:01 +0000

    .

--- src/Main.java
+++ src/Main.java
@@ -22,18 +22,87 @@ public class Main {
         System.out.println("Part two answer: " + partTwoAnswer);
     }
 
+       
     public static int getPartOneNumber(String line) {
-        // do part 1
-        // find the first digit and last digit of the String
-        // return the number put together
-        return 0;
+        char firstNum = '0';
+        char lastNum = '0';
+        for (int i=0; i < line.length(); i++) {
+            if (Character.isDigit(line.charAt(i))) {
+                firstNum = line.charAt(i);
+                break;
+            }
+        }
+
+        for (int i = line.length() - 1; i >= 0; i--) {
+            if (Character.isDigit(line.charAt(i))) {
+                lastNum = line.charAt(i);
+                break;
+            }
+        }
+        return Integer.parseInt("" + firstNum + lastNum);
+    }
+
+    public static int getNumberfromWord(String word) {
+        if ((word.equals("zero")) || (word.equals("orez"))) return 0;
+        if ((word.equals("one")) || (word.equals("eno"))) return 1;
+        if ((word.equals("two")) || (word.equals("owt"))) return 2;
+        if ((word.equals("three")) || (word.equals("eerht"))) return 3;
+        if ((word.equals("four")) || (word.equals("ruof"))) return 4;
+        if ((word.equals("five")) || (word.equals("evif"))) return 5;
+        if ((word.equals("six")) || (word.equals("xis")))  return 6;
+        if ((word.equals("seven")) || (word.equals("neves"))) return 7;
+        if ((word.equals("eight")) || (word.equals("thgie"))) return 8;
+        if ((word.equals("nine")) || (word.equals("enin"))) return 9;
+        return -1; // Invalid word
+    }
+
+    public static String reverseString(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return reversed;
     }
 
     public static int getPartTwoNumber(String line) {
-        // do part 2
-        return 0;
-    }
+        int firstNumber = 12;
+        String[] patterns = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        int bestPosition = Integer.MAX_VALUE; 
+        String bestMatch = null;
+        for (String pattern : patterns) {
+            int position = line.indexOf(pattern);
+            if (position != -1 && position < bestPosition) {
+                bestPosition = position;
+                bestMatch = pattern;
+            }
+        }
+        if (bestMatch.length() == 1) {
+            firstNumber = Character.getNumericValue(bestMatch.charAt(0));
+        } else {
+            firstNumber = getNumberfromWord(bestMatch);
+        }
 
+        int secondNumber = 25; 
+        String reversedLine = reverseString(line);
+        String[] reversedPatterns = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "orez", "eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin"};
+        int bestPositionReversed = Integer.MAX_VALUE; 
+        String bestMatchReversed = null;
+        for (String pattern : reversedPatterns) {
+            int position = reversedLine.indexOf(pattern);
+            if (position != -1 && position < bestPositionReversed) {
+                bestPositionReversed = position;
+                bestMatchReversed = pattern;
+            }
+        }
+        if (bestMatchReversed.length() == 1) {
+            secondNumber = Character.getNumericValue(bestMatchReversed.charAt(0));
+        } else {
+            secondNumber = getNumberfromWord(bestMatchReversed);
+        }
+        int numbersTogether = Integer.parseInt("" + firstNumber + secondNumber);
+        return numbersTogether;
+    }
+    
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
         try {
